e3c5951cb8cd88ac2a744d702b7c02e91253a5bd
jpmanti <jpmanti@gmail.com>
2022-06-27 15:44:34 -0500

    remove ahk start

--- AHKs_Start.bat
+++
@@ -1,3 +0,0 @@
-START C:\Users\jpman\Documents\Code\AHK_Scripts\MediaControls.ahk
-START C:\Users\jpman\Documents\Code\AHK_Scripts\AlwaysOnTop.ahk
-START C:\Users\jpman\Documents\Code\AHK_Scripts\XButtonsConfig.ahk 
